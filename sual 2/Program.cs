446c3aa36b1b204a13d7502f3c6b29fe15bfc5e2
*jeyhun <*jeyhunag@code.edu.az*>
2022-05-29 13:30:08 +0300

    4

---
+++ sual 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace sual_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2, sum = 0;
+            
+            
+                Console.Write("Birinci ededinizi daxil edin: ");
+                number1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ikinci  ededinizi daxil edin: ");
+                number2 = Convert.ToInt32(Console.ReadLine());
+            while (number1<=number2)
+            {
+                if (number1%2!=0)
+                {
+                    
+                    sum=sum+ number1;
+                }
+               number1++;
+              
+            }
+           Console.WriteLine("Toplam:"+sum);
+            
+        }
+    }
+}
